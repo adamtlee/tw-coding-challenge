accb202e9902393faf5ed73293064e15f23c7ae0
adamtlee <adam.lee346@gmail.com>
2023-03-03 13:31:26 -0700

    Added Docs to Logger class

--- TW.DeveloperTest.LogLibrary/Logger.cs
+++ TW.DeveloperTest.LogLibrary/Logger.cs
@@ -10,12 +10,21 @@ namespace TW.DeveloperTest.LogLibrary
            
         }
 
+        /// <summary>
+        /// Logs an informational message to the console with a timestamp.
+        /// </summary>
+        /// <param name="message"> The message to be logged </param>
         public void LogInfo(string message)
         {
             var logMessage = $"[INFO] \n Timestamp: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} \n Message:{message} \n";
             Console.WriteLine(logMessage);
         }
 
+        /// <summary>
+        /// Logs an error message, optionally including the details of an exception.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="ex">The exception that caused the error (optional).</param>
         public void LogError(string message, Exception ex = null)
         {
             var errorMessage = $"[ERROR] \n Timestamp: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} \n Message:{message} \n";
